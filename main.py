4426f90b6385566f0c17c7c4f9ef62f5c10bcaa6
saasin <hdiudhusd@aol.com>
2022-09-20 10:15:18 +0700

    update main

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("this is my firs code on rwid")
-print("ini adalah proyek pertama saya di rwid")
+print("Pergi Ke warang beli 1 Botol susu jika ada telur beli 6 telur")
+print("Baik Bu")
+
+stok_susu = 10
+stok_telur = 10
+
+if stok_susu > 0 and stok_telur > 6:
+    print('Beli Susu 1 dan Telur 6')
+else:
+    print('Pulang Tidak jadi beli')
